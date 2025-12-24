617b6a70d878d1c9365067e1a6b7f080d5f8b512
Akshay Lingamaneni <69821921+akshaylingamaneni@users.noreply.github.com>
2025-12-24 10:35:15 -0600

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,7 +78,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/file-sliders.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
